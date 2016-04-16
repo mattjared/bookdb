d08ce92a764bae0d37720e92323dbe51c9e5220e
Matt Jared <mattjared@matts-mbp-2.attlocal.net>
2016-04-16 14:52:08 -0500

    a bunch of chapters

--- bookdb/urls.py
+++ bookdb/urls.py
@@ -2,13 +2,24 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from collection import views
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete,
+)
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
     url(r'^about/$',TemplateView.as_view(template_name='about.html'),name='about'),
     url(r'^contact/$',TemplateView.as_view(template_name='contact.html'),name='contact'),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^books/(?P<slug>[-\w]+)/$', views.book_detail, name='book_detail'),
     url(r'^books/(?P<slug>[-\w]+)/edit/$', views.edit_book, name='edit_book'),
-    url(r'^accounts/', include('registration.backends.simple.urls'))
+    # The new password reset URLS
+    url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
+    url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',password_reset_confirm,{'template_name':'registration/password_reset_confirm.html'},name="password_reset_confirm"),
+    url(r'^accounts/password/done/$',password_reset_complete,{'template_name': 'registration/password_reset_complete.html'},name="password_reset_complete"),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
