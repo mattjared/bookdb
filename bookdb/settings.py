61b083c842ec3a8e40bea6c9be21d41cc29979dc
Matt Jared <mattjared@matts-mbp-2.attlocal.net>
2016-04-17 20:53:14 -0500

    adding before deploy

--- bookdb/settings.py
+++ bookdb/settings.py
@@ -103,11 +103,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # STATIC_ROOT = 'staticfiles'
