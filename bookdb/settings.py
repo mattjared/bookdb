efa35d9078109e2ff7b169dcb41cdb44582a8a2d
Matt Jared <mattjared@matts-mbp-2.attlocal.net>
2016-04-17 22:39:28 -0500

    adding

--- bookdb/settings.py
+++ bookdb/settings.py
@@ -108,7 +108,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../bookdb/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
