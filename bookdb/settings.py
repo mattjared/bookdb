664a894b638183b6386ff183f9eaf95d5bdfd4b1
Matt Jared <mattjared@matts-mbp-2.attlocal.net>
2016-04-17 20:43:27 -0500

    Committing before pushing to Heroku.

--- bookdb/settings.py
+++ bookdb/settings.py
@@ -98,16 +98,20 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'), 
+    os.path.join(PROJECT_ROOT, 'static'),
 )
+
+# STATIC_ROOT = 'staticfiles'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
