fe69fa0af497b920effb08bb2ccc8a401bea9488
mattjared <mattjared9@gmail.com>
2016-09-18 11:48:19 -0500

    adding new settings

--- bookdb/settings.py
+++ bookdb/settings.py
@@ -103,7 +103,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
